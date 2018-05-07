4cf49c21f5a7acc8550a15d6b9833aeea00eb0be
Nezar <nabdennur@gmail.com>
2018-05-07 03:42:34 -0400

    Be semver compliant and add __all__

--- bbi/__init__.py
+++ bbi/__init__.py
@@ -1,15 +1,18 @@
 #-*- coding: utf-8 -*-
 from __future__ import division, print_function, absolute_import
 from .cbbi import (
-	is_bbi,
-	is_bigwig,
-	is_bigbed,
-	info,
-	zooms,
-	chromsizes,
-	fetch,
-	stackup,
-	fetch_intervals,
+    is_bbi,
+    is_bigwig,
+    is_bigbed,
+    info,
+    zooms,
+    chromsizes,
+    fetch,
+    stackup,
+    fetch_intervals,
 )
 
-__version__ = '0.1'
+__all__ = ['is_bbi', 'is_bigwig', 'is_bigbed', 'info', 'zooms', 'chromsizes', 
+           'fetch', 'stackup', 'fetch_intervals']
+
+__version__ = '0.1.0'
