5f2a83ced671a468d0a9a2ddfbabfa7daf7d162c
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-06-04 21:38:15 -0400

    Test

--- tests/test_bbi.py
+++ tests/test_bbi.py
@@ -120,6 +120,10 @@ def test_fetch_summary_stats(path):
         path, 'chr21', 20000000, 20001000, bins=10, summary='max'
     ).max()
     assert np.isclose(vmax, np.max(values))
+    vsum = bbi.fetch(
+        path, 'chr21', 20000000, 20001000, bins=10, summary='sum'
+    ).sum()
+    assert np.isclose(vsum, np.sum(values))
 
     with pytest.raises(ValueError):
         bbi.fetch(path, 'chr21', 20000000, 20001000, bins=10, summary='foo')
