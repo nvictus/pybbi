9fa409ce87e044442f6e721580e6bcc3c66076ea
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-06-11 18:02:23 -0400

    Actually do sum in test

--- tests/test_bbi.py
+++ tests/test_bbi.py
@@ -120,7 +120,7 @@ def test_fetch_summary_stats(path):
         path, 'chr21', 20000000, 20001000, bins=10, summary='max'
     ).max()
     assert np.isclose(vmax, np.max(values))
-    vsum = bbi.fetch(path, 'chr21', 20000000, 20001000, bins=1000).sum()
+    vsum = bbi.fetch(path, 'chr21', 20000000, 20001000, bins=100, summary='sum').sum()
     assert np.isclose(vsum, np.sum(values))
 
     with pytest.raises(ValueError):
